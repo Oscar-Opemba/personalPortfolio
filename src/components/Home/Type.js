dbea574fafc28233058d7880856c42b6f112a878
Oscar-Opemba <oscarfx94@gmail.com>
2025-10-05 01:27:29 +0300

    Updated typewriter roles with SOC Analyst and cyber titles

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 
+import React from "react";
+import Typewriter from "typewriter-effect";
+
 function Type() {
   return (
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+              "A Cybersecurity Researcher 🧠",
+              "A SOC Analyst 🕵️‍♂️",
+              "A Bug Bounty Hunter 💰",
+              "A Network Security Specialist 🌐",
+              "An Ethical Hacker ⚡",
+              "A Penetration Tester 💻",
+              "A Threat Intelligence Explorer 🔍",
+              "A Blue Team Defender 🛡️",
+              "A Red Team Operator 🚀",
+              "A Digital Forensics Enthusiast 🔬"
         ],
         autoStart: true,
         loop: true,
