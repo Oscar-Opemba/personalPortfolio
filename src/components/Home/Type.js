1717947d7db3d3b8f215e9be3615cfe4b8cab3a9
Oscar-Opemba <oscarfx94@gmail.com>
2025-10-05 10:30:20 +0300

    Updated Type.js animations

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,26 +1,22 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 
-import React from "react";
-import Typewriter from "typewriter-effect";
-
 function Type() {
   return (
     <Typewriter
       options={{
         strings: [
-          
           "A Cybersecurity Researcher 🧠",
-          "A SOC Analyst 🕵️‍♂️",
-          "A Bug Bounty Hunter 💰",
-          "A Network Security Specialist 🌐",
-          "An Ethical Hacker ⚡",
-          "A Penetration Tester 💻",
-          "A Threat Intelligence Explorer 🔍",
-          "A Blue Team Defender 🛡️",
-          "A Red Team Operator 🚀",
-          "A Digital Forensics Enthusiast 🔬"
-        ],
+          "SOC Analyst 🕵️‍♂️",
+          "Bug Bounty Hunter 💰",
+          "Penetration Tester 💻",
+          "Ethical Hacker ⚡",
+          "Threat Intelligence Analyst 🔍",
+          "Blue Team Defender 🛡️",
+          "Red Team Operator 🚀",
+          "Digital Forensics Enthusiast 🔬",
+          "Network Security Specialist 🌐"
+        ],  
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
@@ -30,3 +26,4 @@ function Type() {
 }
 
 export default Type;
+
