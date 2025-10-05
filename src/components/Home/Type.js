d308f15132fdcd4720484635120a689f752eee91
Oscar Opemba <146231910+Oscar-Opemba@users.noreply.github.com>
2025-10-05 09:47:36 +0300

    Remove duplicate strings from Typewriter component
    Removed duplicate strings in Typewriter options.

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -9,16 +9,17 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-              "A Cybersecurity Researcher 🧠",
-              "A SOC Analyst 🕵️‍♂️",
-              "A Bug Bounty Hunter 💰",
-              "A Network Security Specialist 🌐",
-              "An Ethical Hacker ⚡",
-              "A Penetration Tester 💻",
-              "A Threat Intelligence Explorer 🔍",
-              "A Blue Team Defender 🛡️",
-              "A Red Team Operator 🚀",
-              "A Digital Forensics Enthusiast 🔬"
+          
+          "A Cybersecurity Researcher 🧠",
+          "A SOC Analyst 🕵️‍♂️",
+          "A Bug Bounty Hunter 💰",
+          "A Network Security Specialist 🌐",
+          "An Ethical Hacker ⚡",
+          "A Penetration Tester 💻",
+          "A Threat Intelligence Explorer 🔍",
+          "A Blue Team Defender 🛡️",
+          "A Red Team Operator 🚀",
+          "A Digital Forensics Enthusiast 🔬"
         ],
         autoStart: true,
         loop: true,
